2d14a70334989b283e7497838388860caa61c17d
gui10l1 <guiribeiro2003@gmail.com>
2025-10-02 11:13:57 -0300

    add: register farmer module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { FarmersModule } from './farmers/farmers.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DatabaseModule.forRoot(), FarmersModule],
   controllers: [AppController],
   providers: [AppService],
 })
