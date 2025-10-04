19949fc99224efb796387077caf4ed9222158e50
gui10l1 <guiribeiro2003@gmail.com>
2025-10-04 01:55:06 -0300

    add: rest of farms methods and unit tests - service

--- src/farms/farms.service.spec.ts
+++ src/farms/farms.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FarmsService } from './farms.service';
-import { FarmDTO } from './dtos';
+import { FarmDTO, UpdateFarmDTO } from './dtos';
 import ApiError from 'src/errors/ApiError';
 import { Provider } from '@nestjs/common';
 import { FARMER_REPOSITORY_PROVIDER_ID } from 'src/farmers/constants';
@@ -12,6 +12,7 @@ import { mockedFarmer } from '../../mocks/farmers';
 describe('FarmsService', () => {
   let service: FarmsService;
   let farmersRepository: FakeFarmersRepository;
+  let farmsRepository: FakeFarmsRepository;
 
   beforeEach(async () => {
     const providers: Provider[] = [
@@ -32,6 +33,9 @@ describe('FarmsService', () => {
     service = module.get<FarmsService>(FarmsService);
     farmersRepository = module.get<FakeFarmersRepository>(
       FARMER_REPOSITORY_PROVIDER_ID,
+    );
+    farmsRepository = module.get<FakeFarmsRepository>(
+      FARM_REPOSITORY_PROVIDER_ID,
     );
   });
 
@@ -88,6 +92,139 @@ describe('FarmsService', () => {
       expect(farm).toHaveProperty('id');
       expect(farm.id).toBe(1);
       expect(farm.farmer_id).toBe(data.farmerId);
+      expect(farm.name).toBe(data.name);
+      expect(farm.agricultural_area).toBe(data.agriculturalArea);
+      expect(farm.vegetation_area).toBe(data.vegetationArea);
+      expect(farm.total_area).toBe(data.totalArea);
+      expect(farm.city).toBe(data.city);
+      expect(farm.state).toBe(data.state);
+    });
+  });
+
+  describe('List Farms By Farmer Id', () => {
+    it('should not be able to list farms from a non-existing farmer', async () => {
+      const randomId = Math.floor(Math.random() * 100);
+
+      await expect(service.listByFarmerId(randomId)).rejects.toBeInstanceOf(
+        ApiError,
+      );
+    });
+
+    it("should be able to list all farmer's farms", async () => {
+      const farmerId = 1;
+      const mockFarms = [
+        {
+          agriculturalArea: 50,
+          city: 'City',
+          farmerId: farmerId,
+          name: 'Farm',
+          state: 'ST',
+          totalArea: 100,
+          vegetationArea: 50,
+        },
+        {
+          agriculturalArea: 50,
+          city: 'City',
+          farmerId: farmerId,
+          name: 'Farm',
+          state: 'ST',
+          totalArea: 100,
+          vegetationArea: 50,
+        },
+        {
+          agriculturalArea: 50,
+          city: 'City',
+          farmerId: 2,
+          name: 'Farm',
+          state: 'ST',
+          totalArea: 100,
+          vegetationArea: 50,
+        },
+      ];
+
+      await Promise.all(
+        mockFarms.map(async (farm) => {
+          await farmsRepository.create(farm);
+        }),
+      );
+
+      jest.spyOn(farmersRepository, 'findById').mockResolvedValue(mockedFarmer);
+
+      const list = await service.listByFarmerId(farmerId);
+
+      expect(list.length).toBe(2);
+    });
+  });
+
+  describe('Delete Farms', () => {
+    it('should not be able to delete non-existing farms', async () => {
+      await expect(service.delete(12345)).rejects.toBeInstanceOf(ApiError);
+    });
+
+    it('should be able to delete farms', async () => {
+      const spyOn = jest.spyOn(farmsRepository, 'delete');
+      const id = 1;
+
+      const farm = await farmsRepository.create({
+        agriculturalArea: 50,
+        city: 'City',
+        farmerId: 1,
+        name: 'Farm',
+        state: 'ST',
+        totalArea: 100,
+        vegetationArea: 50,
+      });
+
+      await service.delete(id);
+
+      expect(spyOn).toHaveBeenCalledWith(farm);
+      expect(spyOn).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('Update Farms', () => {
+    it('should not be able to update non-existing farms', async () => {
+      await expect(service.delete(12345)).rejects.toBeInstanceOf(ApiError);
+    });
+
+    it('should not be able to update farms when total area is minor than agricultural and vegetation areas combined', async () => {
+      const data: FarmDTO = {
+        agriculturalArea: 10 * HEC_IN_METERS,
+        vegetationArea: 15 * HEC_IN_METERS,
+        totalArea: 20 * HEC_IN_METERS,
+        city: 'City',
+        farmerId: 1,
+        name: 'Farm',
+        state: 'ST',
+      };
+
+      await expect(service.create(data)).rejects.toBeInstanceOf(ApiError);
+    });
+
+    it('should be able to update farms', async () => {
+      const id = 1;
+      const data: UpdateFarmDTO = {
+        agriculturalArea: 20 * HEC_IN_METERS,
+        vegetationArea: 20 * HEC_IN_METERS,
+        totalArea: 40 * HEC_IN_METERS,
+        city: 'Updated City',
+        name: 'Updated Farm',
+        state: 'UT',
+      };
+
+      const createdFarm = await farmsRepository.create({
+        agriculturalArea: 50,
+        city: 'City',
+        farmerId: 1,
+        name: 'Farm',
+        state: 'ST',
+        totalArea: 100,
+        vegetationArea: 50,
+      });
+
+      const farm = await service.update(createdFarm.id, data);
+
+      expect(farm.id).toBe(id);
       expect(farm.name).toBe(data.name);
       expect(farm.agricultural_area).toBe(data.agriculturalArea);
       expect(farm.vegetation_area).toBe(data.vegetationArea);
--- src/farms/farms.service.ts
+++ src/farms/farms.service.ts
@@ -2,7 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { FARM_REPOSITORY_PROVIDER_ID } from './constants';
 import type { IFarmsRepository } from './interfaces/repositories.interface';
 import Farm from './entities/farm.entity';
-import { FarmDTO } from './dtos';
+import { FarmDTO, UpdateFarmDTO } from './dtos';
 import { FARMER_REPOSITORY_PROVIDER_ID } from 'src/farmers/constants';
 import type { IFarmersRepository } from 'src/farmers/interfaces/repositories.interface';
 import ApiError from 'src/errors/ApiError';
@@ -35,5 +35,39 @@ export class FarmsService {
     const farm = await this.farmsRepository.create(data);
 
     return farm;
+  }
+
+  public async listByFarmerId(farmerId: number): Promise<Farm[]> {
+    const farm = await this.farmersRepository.findById(farmerId);
+
+    if (!farm) throw new ApiError('Fazenda não encontrada!');
+
+    return this.farmsRepository.listByFarmerId(farmerId);
+  }
+
+  public async update(farmId: number, data: UpdateFarmDTO): Promise<Farm> {
+    const farm = await this.farmsRepository.findById(farmId);
+
+    if (!farm) throw new ApiError('Fazenda não encontrada!');
+
+    if (data.agriculturalArea && data.vegetationArea) {
+      const totalAreaCalculated = data.agriculturalArea + data.vegetationArea;
+
+      if (totalAreaCalculated > (data.totalArea || farm.total_area)) {
+        throw new ApiError(
+          'A soma das áreas agrícolas e de vegetação é maior do que a área total!',
+        );
+      }
+    }
+
+    return this.farmsRepository.update(farm, data);
+  }
+
+  public async delete(farmId: number): Promise<void> {
+    const farm = await this.farmsRepository.findById(farmId);
+
+    if (!farm) throw new ApiError('Fazenda não encontrada!');
+
+    await this.farmsRepository.delete(farm);
   }
 }
