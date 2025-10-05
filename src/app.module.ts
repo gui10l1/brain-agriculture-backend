d4775a1998e4a010538e7d1b18bb70683cf244f2
gui10l1 <guiribeiro2003@gmail.com>
2025-10-05 13:15:12 -0300

    remove: app module service, controller, and unit e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { FarmersModule } from './farmers/farmers.module';
 import { FarmsModule } from './farms/farms.module';
 import { CropYieldsModule } from './crop-yields/crop-yields.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, FarmersModule, FarmsModule, CropYieldsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    FarmersModule,
+    FarmsModule,
+    CropYieldsModule,
+  ],
 })
 export class AppModule {}
