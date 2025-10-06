b350e5160a3bd05b750521016a77e3e89aa03e08
gui10l1 <guiribeiro2003@gmail.com>
2025-10-06 13:32:20 -0300

    fix: cors connection

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: { origin: '*' } });
 
   const config = new DocumentBuilder()
     .setTitle('Farmers Management')
